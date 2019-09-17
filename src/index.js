0b7fce2c333a19a927e3a0ec7ee5eec5514ed243
Alex Johnston <alexjohnston@ymail.com>
2019-09-17 14:38:03 +0100

    initial changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const BASE_URL = 'http://swapi.co/api'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const PEOPLE_URL = `${BASE_URL}/people`
+const STARSHIPS_URL = `${BASE_URL}/starships`
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let playerScore = 0
+let opponentScore = 0
+
+let currentPlayerPerson = {}
+let currentOpponentPerson = {}
+
+// Find the buttons
+
+const newPeopleGameButton = document.querySelector('#new-people-button')
+const newStarshipsGameButton = document.querySelector('#new-starships-button')
+
+// Add event listeners for new game buttons
+
+newPeopleGameButton.addEventListener('click', () => newGame('people'))
+newStarshipsGameButton.addEventListener('click', () => newGame('starships'))
+
+// Find the cards
+
+const playerCard = document.querySelector('#player-card')
+const opponentCard = document.querySelector('#opponent-card')
+
+const newGame = (type) => {
+
+  if (type === 'people') {
+    selectPeopleCard()
+  } else {
+    // selectStarshipCard()
+  }
+}
+
+const selectPeopleCard = () => {
+  const playerRandomPerson = getPerson(Math.floor(Math.random() * 87))
+  const opponentRandomPerson = getPerson(Math.floor(Math.random() * 87))
+  if (playerRandomPerson === opponentRandomPerson) {
+    selectPeopleCard()
+  } else {
+    renderCards(playerRandomPerson, opponentRandomPerson)
+  }
+}
+
+const getPerson = (id) => {
+  fetch(`${PEOPLE_URL}/${id}`)
+  .then(resp => resp.json())
+}
+
+const renderCards = (playerPerson, opponentPerson) => {
+  currentPlayerPerson = playerPerson
+  currentOpponentPerson = opponentPerson
+  debugger
+}
