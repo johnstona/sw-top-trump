7ebfe3cf896cd66527e431e8da66eef1eb0744a7
Alex Johnston <alexjohnston@ymail.com>
2019-09-17 17:25:18 +0100

    Top Trump functionality added

--- src/index.js
+++ src/index.js
@@ -1,55 +1,173 @@
-const BASE_URL = 'http://swapi.co/api'
+const BASE_URL = 'https://swapi.co/api'
 
 const PEOPLE_URL = `${BASE_URL}/people`
 const STARSHIPS_URL = `${BASE_URL}/starships`
 
-let playerScore = 0
-let opponentScore = 0
+let player1Score = 0
+let player2Score = 0
 
-let currentPlayerPerson = {}
-let currentOpponentPerson = {}
+const player1ScoreElement = document.querySelector('#player1-score')
+const player2ScoreElement = document.querySelector('#player2-score')
+const player1CardElement = document.querySelector('#player1-card-body')
+const player2CardElement = document.querySelector('#player2-card-body')
+
+let player1Current = {}
+let player2Current = {}
 
 // Find the buttons
 
 const newPeopleGameButton = document.querySelector('#new-people-button')
 const newStarshipsGameButton = document.querySelector('#new-starships-button')
+const nextCardButton = document.querySelector('#next-card-button')
 
-// Add event listeners for new game buttons
+// Add event listeners for buttons
 
 newPeopleGameButton.addEventListener('click', () => newGame('people'))
 newStarshipsGameButton.addEventListener('click', () => newGame('starships'))
 
-// Find the cards
-
-const playerCard = document.querySelector('#player-card')
-const opponentCard = document.querySelector('#opponent-card')
+// New Game function depending on type of game
 
 const newGame = (type) => {
-
+  newPeopleGameButton.disabled = true
+  newStarshipsGameButton.disabled = true
+  nextCardButton.classList.remove('invisible')
+  nextCardButton.disabled = true
+  nextCardButton.addEventListener('click', () => nextCard(type))
   if (type === 'people') {
-    selectPeopleCard()
+    selectPeopleCard('player1')
+    selectPeopleCard('player2')
   } else {
-    // selectStarshipCard()
+    selectStarshipCard('player1')
+    selectStarshipCard('player2')
   }
 }
 
-const selectPeopleCard = () => {
-  const playerRandomPerson = getPerson(Math.floor(Math.random() * 87))
-  const opponentRandomPerson = getPerson(Math.floor(Math.random() * 87))
-  if (playerRandomPerson === opponentRandomPerson) {
-    selectPeopleCard()
+// Functions for selecting cards
+
+const selectPeopleCard = (player) => {
+  let random = Math.floor(Math.random() * 87)
+  getPerson(random, player).then(person => renderCard(person, player, 'person'))
+}
+
+const selectStarshipCard = (player) => {
+  let random = Math.floor(Math.random() * 35)
+  getStarship(random, player).then(starship => renderCard(starship, player, 'starship'))
+}
+
+// Next card function - only available after a selection
+
+const nextCard = (type) => {
+  player2CardElement.classList.add('invisible')
+  nextCardButton.disabled = true
+  if (type === 'people') {
+    selectPeopleCard('player1')
+    selectPeopleCard('player2')
   } else {
-    renderCards(playerRandomPerson, opponentRandomPerson)
+    selectStarshipCard('player1')
+    selectStarshipCard('player2')
   }
 }
 
-const getPerson = (id) => {
-  fetch(`${PEOPLE_URL}/${id}`)
-  .then(resp => resp.json())
+// Fetch requests for people and starships
+
+const getPerson = (id, player) => {
+  return fetch(`${PEOPLE_URL}/${id}`)
+    .then(resp => resp.json())
+    .then(person => {
+      if (person.name && ((person.height !== 'unknown') && (person.mass !== 'unknown'))) return person
+      else {
+        selectPeopleCard(player)
+      }
+    })
 }
 
-const renderCards = (playerPerson, opponentPerson) => {
-  currentPlayerPerson = playerPerson
-  currentOpponentPerson = opponentPerson
-  debugger
+const getStarship = (id, player) => {
+  return fetch(`${STARSHIPS_URL}/${id}`)
+    .then(resp => resp.json())
+    .then(starship => {
+      if (starship.name && ((starship.length !== 'unknown') && (starship.crew !== 'unknown'))) return starship
+      else {
+        selectStarshipCard(player)
+      }
+    })
+}
+
+// Render function that works for both people and starships
+
+const renderCard = (data, player, type) => {
+  if (!data) return
+  updateCurrent(data, player, type)
+  let firstOption
+  let secondOption
+  if (type === 'person') {
+    firstOption = 'height'
+    secondOption = 'mass'
+  } else {
+    firstOption = 'length'
+    secondOption = 'crew'
+  }
+  let card = document.querySelector(`#${player}-card-body`)
+  card.innerHTML = ''
+  let cardTitle = document.createElement('h5')
+  cardTitle.innerText = data.name
+  let opt1 = document.createElement('p')
+  opt1.innerText = `${firstOption}: ${data[firstOption]}`
+  if (player === 'player1') {
+    let opt1Button = document.createElement('button')
+    opt1Button.innerText = `Choose ${firstOption}`
+    opt1Button.addEventListener('click', () => select(firstOption))
+    opt1.append(opt1Button)
+  }
+  let opt2 = document.createElement('p')
+  opt2.innerText = `${secondOption}: ${data[secondOption]}`
+
+  if (player === 'player1') {
+    let opt2Button = document.createElement('button')
+    opt2Button.innerText = `Choose ${secondOption}`
+    opt2Button.addEventListener('click', () => select(secondOption))
+    opt2.append(opt2Button)
+  }
+
+  card.append(cardTitle, opt1, opt2)
+}
+
+// Updates global value of current cards to check result
+
+const updateCurrent = (data, player) => {
+  if (player === 'player1') {
+    player1Current = data
+  } else {
+    player2Current = data
+  }
+}
+
+// Selection and checking result
+
+const select = (option) => {
+  player2CardElement.classList.remove('invisible')
+  const selectButtons = player1CardElement.querySelectorAll('button')
+  selectButtons.forEach(button => {
+    button.classList.add('invisible')
+  })
+  const lastResult = document.querySelector('#last-result')
+  nextCardButton.disabled = false
+  if (parseInt(player1Current[option]) > parseInt(player2Current[option])) {
+    lastResult.innerText = 'Victory!'
+    player1Score += 1
+  } else if (parseInt(player1Current[option]) < parseInt(player2Current[option])) {
+    lastResult.innerText = 'Loss!'
+    player2Score += 1
+  } else if (parseInt(player1Current[option]) === parseInt(player2Current[option])) {
+    lastResult.innerText = 'Draw!'
+  } else {
+    window.alert('Something went wrong!')
+  }
+  updateScore()
+}
+
+// Updates the score in the DOM
+
+const updateScore = () => {
+  player1ScoreElement.innerText = player1Score
+  player2ScoreElement.innerText = player2Score
 }
